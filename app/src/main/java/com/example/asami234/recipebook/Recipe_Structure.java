4dbb601abcce6d14bc559e71e2921ccaa3a5f42f
asami234 <asami1997@gmail.com>
2018-11-24 11:14:24 +0800

    Initial commit and created a class containing the structure of elements in the DB.

---
+++ app/src/main/java/com/example/asami234/recipebook/Recipe_Structure.java
@@ -0,0 +1,38 @@
+package com.example.asami234.recipebook;
+
+public class Recipe_Structure {
+
+    private int recipe_id;
+    private String recipe_title;
+    private String recipe_content;
+
+
+    //  main constructor used to set  all the contents
+    public Recipe_Structure(int recipe_id, String recipe_title, String recipe_content) {
+        this.recipe_id = recipe_id;
+        this.recipe_title = recipe_title;
+        this.recipe_content = recipe_content;
+    }
+
+
+     // set recipe id only
+
+      private void setRecipe_id(int id){
+        this.recipe_id = id;
+      }
+
+
+      // set recipe content only
+      private void setRecipe_content(String content){
+        this.recipe_content = content;
+      }
+
+
+      // set recipe title only
+      private void setRecipe_title(String title){
+        this.recipe_title = title;
+      }
+
+
+
+}
